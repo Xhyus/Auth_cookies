5ccf7f507252d8ba476251198720a754e7bdd52d
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-12-10 13:34:03 -0300

    Se genera archivo index.js

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+
+app.use(express.json());
+app.use(cors());
+app.options('*', cors());
+app.use('/api', productRoutes);
+app.use('/api', userRoutes);
+
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect(process.env.DB, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Connected to database");
+    }
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+})
